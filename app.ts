1bc810fea0231cc0919d40342aaf497f4ed7ef59
Nabil-Mo7amed <nmh20209@gmail.com>
2024-08-25 18:28:53 +0300

    first commit

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+const app: express.Application = express()
+dotenv.config();
+app.use(express.json())
+
+
+
+app.get('/', function (req:express.Request,res:express.Response){
+    res.json({Message:'ElHamd Lilah'})
+} )
+
+mongoose.connect(process.env.DB!).then(()=>{
+    console.log("Data Base Connect");
+    
+}).catch((err:Error)=>{
+    console.log(err);
+    
+})
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
